0b7a048bb24e415b3a05e9023e8de438e0a9eda3
GreenNeptune <silentwisdom75@gmail.com>
2020-11-08 21:56:47 +0200

    Replace Avatar image url

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,7 +10,7 @@ function Sidebar() {
   return (
     <div className='sidebar'>
       <div className="sidebar__header">
-        <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" />
+        <Avatar src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
