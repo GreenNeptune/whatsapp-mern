dd16b327e56afaf986aef2bc2848d6c24d03d809
GreenNeptune <silentwisdom75@gmail.com>
2020-11-12 20:13:35 +0200

    Add Chat body

--- src/Chat.js
+++ src/Chat.js
@@ -24,6 +24,72 @@ function Chat() {
           </IconButton>
         </div>
       </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Jack</span>
+          This is a message ddddddddddddddddlonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggfffffffffffffffffffffffffff
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Jack</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
