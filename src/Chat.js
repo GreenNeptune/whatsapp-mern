4b788cc5ae87b64f9485b82dffbe8bc00a0d1163
GreenNeptune <silentwisdom75@gmail.com>
2020-11-17 17:05:18 +0200

    Add Chat footer

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, Mic as MicIcon, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
   return (
@@ -91,7 +91,20 @@ function Chat() {
           </span>
         </p>
       </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            placeholder="Type a message"
+            type="text" />
+          <button type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
+
   )
 }
 
