e88eab085f6fecffc1fd57119ba990fd8961facc
GreenNeptune <silentwisdom75@gmail.com>
2020-11-18 18:52:28 +0200

    Add POST /api/v1/messages/new Route

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,16 +1,31 @@
 require('dotenv').config();
-const connectDB = require('./config/DB');
-connectDB();
-
 const express = require('express');
 const mongoose = require('mongoose');
+const Messages = require('./dbMessages');
+const connectDB = require('./config/DB');
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 9000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.status(200).send("Hello world");
 })
+
+app.post('/api/v1/messages/new', (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(`new messages created: \n${data}`);
+    }
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Listen on localhost:${port}`);
