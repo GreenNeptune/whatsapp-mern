47d1962fa8e274db6d2c0b59e9c00c8ea002dfe1
GreenNeptune <silentwisdom75@gmail.com>
2020-11-18 23:22:14 +0200

    Get messages with axios and pusher

--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from "pusher-js";
+import axios from './axios';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/v1/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher('ed7e2b4f3e969c20bc48', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function (newMessage) {
+      setMessages([...messages, newMessage])
+    });
+
+    return () => {
+      channel.unbind()
+      channel.unsubscribe();
+    }
+  }, [messages])
+
   return (
     <div className="app">
       <div className="app__body">
